b4f49cdb1933d0cb784af67d4c4ebc0270bb7f7a
Joao <joao.vot@hotmail.com>
2025-05-30 16:49:17 -0300

    first commit

---
+++ src/test/java/dev/joao/UserRegister/UserRegisterApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.joao.UserRegister;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserRegisterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
